6e666332a5cc33438590a56f67701a5ded07eaa8
Martins Ceske <Martinsh_Ceske@rcomext.com>
2019-11-13 14:49:40 +0200

    hw6 izlabots

--- src/lv/jg/lesson6/homework1/NumberService.java
+++ src/lv/jg/lesson6/homework1/NumberService.java
@@ -2,22 +2,29 @@ package lv.jg.lesson6.homework1;
 
 public class NumberService {
 
+	//šajā klasē nekādu stāvokli nav nepieciešams glabāt
+	//jo metode paredz ka šie stāvokļiem ir jābūt jauniem, kad tās (metodes) tiek izsauktas
 	private int boundary1, boundary2;
 	private int i, j;
 	private int sum, evenNumberCount;
-	
+
+	//robežām ir jābūt nodotām kā parametriem "sumInRange" un "getEvenNumberCount" klasē
 	NumberService (int boundary1, int boundary2) {
 		this.boundary1 = boundary1;
 		this.boundary2 = boundary2;
 	}
-	
+
+	//metodei ir jātdod rezultāts, nevis jāizvada konsolē - to var darīt tests
 	public void sumInRange() {
 		if (boundary1 <= boundary2) {
 			i = boundary1;
 			j = boundary2;
+			//tā kā šī daļa mums atkātojas, tad to varam iznest metodē
+			//kurai varam padot 2 mainīgos
+			//tas nozīmē, ka ar if varma noteikt kādā secībā iedodam tos
 			while (i <= j) {
-				sum = sum + i;	
-				i++; 
+				sum = sum + i;	// lietojam īso pierakstu "sum =+ i"
+				i++;
 			}
 		} else {
 			i = boundary1;
@@ -29,7 +36,8 @@ public class NumberService {
 		}
 		System.out.println("Sum = " + sum + "; ");
 	}
-	
+
+	//skatīt iepriekšējās metodes komentārus
 	public void getEvenNumberCount() {
 		if (boundary1 <= boundary2) {
 			i = boundary1;
--- src/lv/jg/lesson6/homework1/NumberServiceTest.java
+++ src/lv/jg/lesson6/homework1/NumberServiceTest.java
@@ -1,7 +1,8 @@
 package lv.jg.lesson6.homework1;
 
 public class NumberServiceTest {
-	
+
+	//vajadzētu lietot tomēr atsevišķās metodes ar nosaukumiem, kas apraksta katru teesta gadījumu
 	public static void main(String[] args) {
 		
 		NumberService myRange = new NumberService(2,5);
