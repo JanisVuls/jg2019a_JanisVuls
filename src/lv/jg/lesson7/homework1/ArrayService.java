7523eb142eecec8e3df4d2db514ff58d603ce1c0
Martins Ceske <Martinsh_Ceske@rcomext.com>
2019-11-19 16:07:44 +0200

    hw7 izlabots

--- src/lv/jg/lesson7/homework1/ArrayService.java
+++ src/lv/jg/lesson7/homework1/ArrayService.java
@@ -2,11 +2,13 @@ package lv.jg.lesson7.homework1;
 
 import java.util.Random;
 
+//lietojam ctrl + alt + l
 public class ArrayService {
 
+    //šis konstruktors nav nepieciešams
     ArrayService () {
     }
-    
+
     public int[] create(int size) {
     	 return new int[size];
     	 }
@@ -15,12 +17,12 @@ public class ArrayService {
     public void fillArrayWithRandomNumbers(int[] array) {
     	 Random randomGenerator = new Random();
     	  for (int i = 0; i < array.length; i++) {
-    	 array[i] = randomGenerator.nextInt(101);
+    	 array[i] = randomGenerator.nextInt(101); //"101" jāiznes kā konstante
     	 }
     }
 
     public void printArrayToConsole(int[] array) {
-    	    System.out.print("Numbers in array: "); 
+    	    System.out.print("Numbers in array: ");
     	for (int numbersInArray : array) {
     		System.out.print(numbersInArray+ ", ");
     	 }
@@ -28,7 +30,7 @@ public class ArrayService {
 
     public void sumInArray(int[] array) {
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {  //pēs iespējas ir jāizmanto "forEach" pieraksts
             sum += array[i];
         }
         System.out.println("Sum = " + sum + "; ");
