4d7d7b03e1d777a44815b8455e3b6eb5b79b4beb
JanisVuls <vuls.janis@gmail.com>
2019-12-09 22:06:41 +0200

    Lesson 10

---
+++ src/lv/jg/lesson10/homework1/User.java
@@ -0,0 +1,49 @@
+package lv.jg.lesson10.homework1;
+
+public class User {
+
+	private String name;
+	private String lastName;
+	int age;
+	
+	public User(String name, String lastName, int age) {
+        if (!isValidName(name)) {
+            throw new IllegalArgumentException("Wrong input value for Name");
+        }
+        this.name = name;
+        
+        if (!isValidLastName(lastName)) {
+            throw new IllegalArgumentException("Wrong input value for Last Name");
+        }
+        this.lastName = lastName;
+        
+        if (!isValidAge(age)) {
+            throw new IllegalArgumentException("Wrong input value for Age");
+        }
+        this.age = age;
+    }
+
+    private boolean isValidName(String name) {
+        return name.length() >= 3 && name.length() <= 15;
+    }
+    
+    private boolean isValidLastName(String lastName) {
+        return lastName.length() >= 3 && lastName.length() <= 15;
+    }
+    
+    private boolean isValidAge(int age) {
+        return age >= 0 && age <= 120;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+	
+    public String getLastName() {
+        return this.lastName;
+    }
+    
+    public int getAge() {
+        return this.age;    
+    }
+}
---
+++ src/lv/jg/lesson10/homework1/UserValidationService.java
@@ -0,0 +1,39 @@
+package lv.jg.lesson10.homework1;
+
+public class UserValidationService {
+
+	public static void main(String[] args) {
+        shouldCreateUser();
+        shouldFailIfInputValueToSmall();
+        shouldFailIfInputValueToLarge();
+}
+
+	private static void shouldCreateUser() {
+        User user = new User("Maksims", "Maisins", 10);
+	}
+
+	private static void shouldFailIfInputValueToSmall() {
+        System.out.println("================");
+        boolean result = false;
+        try {
+        	User user = new User("Ma", "Maisins", 0);
+        } catch (IllegalArgumentException ex) {
+            result = true;
+        }
+        System.out.println("Test: shouldFailIfInputValueToSmall");
+        System.out.println(result ? "SUCCESS" : "FAILED");
+    }
+		
+	private static void shouldFailIfInputValueToLarge() {
+        System.out.println("================");
+        boolean result = false;
+        try {
+        	User user = new User("Maksims", "Maisins123456789", 120);
+        } catch (IllegalArgumentException ex) {
+            result = true;
+        }
+        System.out.println("Test: shouldFailIfInputValueToLarge");
+        System.out.println(result ? "SUCCESS" : "FAILED");
+    }
+
+	}
