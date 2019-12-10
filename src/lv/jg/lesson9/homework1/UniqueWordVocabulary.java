57562a014366660ffbc81be72e6270e77dcf4bab
JanisVuls <vuls.janis@gmail.com>
2019-12-10 22:52:20 +0200

    Lesson 9 correction

--- src/lv/jg/lesson9/homework1/UniqueWordVocabulary.java
+++ src/lv/jg/lesson9/homework1/UniqueWordVocabulary.java
@@ -1,41 +1,30 @@
 package lv.jg.lesson9.homework1;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
-//Nav izpildīti uzdevuma nosacījumi
 public class UniqueWordVocabulary {
-
 	public static void main(String[] args) {
 
-		int UniqueWords = 0;
-
-		        Set<String> vocabulary = new HashSet<>();
+		Set<String> vocabulary = new HashSet<>();
 		        addWord(vocabulary);
-				printToConsole();
 		        getUniqueWords(vocabulary);
-				System.out.println(vocabulary);
-		        System.out.println("UniqueWords = " + UniqueWords);
-		    }
+		        printToConsole(vocabulary);
+    		    }
 
 		    private static void addWord(Set<String> set) {
 		        set.add("Kazis");
 		        set.add("Jomo");
 		        set.add("Jolo");
+		        set.add("Jolo");
 		    }
-
+		    
 		    private static void getUniqueWords(Set<String> set) {
-//				if (Set<String> set) {
-//
-//				}
+		    	System.out.println("UniqueWords = " + set.size()); 
+		    	}
 
-		    }
-
-			private static void printToConsole() {
-//				System.out.println(vocabulary);
-//				System.out.println("UniqueWords = " + UniqueWords);
-
-			}
-
+		    private static void printToConsole(Set<String> vocabulary) {
+		    	System.out.println(vocabulary);
+		      }
+		    
 }
